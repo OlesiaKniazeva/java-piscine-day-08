951d27d316de4b2f8d36bb0ee0a07f75911a8704
Madeleine Yael <myael@student.21-school.ru>
2022-07-29 17:51:57 +0500

    remove unnessesary sql files

--- ex01/Service/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users(email) VALUES ('oleh@gmail.com');
-INSERT INTO users(email) VALUES ('kot@gmail.com');
-INSERT INTO users(email) VALUES ('loko@gmail.com');
-INSERT INTO users(email) VALUES ('nouyt@gmail.com');
-INSERT INTO users(email) VALUES ('dfg66@gmail.com');
-INSERT INTO users(email) VALUES ('rewq00@gmail.com');
-INSERT INTO users(email) VALUES ('byurv@gmail.com');
-INSERT INTO users(email) VALUES ('nymve@gmail.com');
--- ex01/Service/src/main/resources/shema.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS users CASCADE ;
-
-CREATE TABLE IF NOT EXISTS users (
-    id          SERIAL PRIMARY KEY,
-    email       VARCHAR(100)
-);
