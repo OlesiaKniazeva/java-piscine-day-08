e37272e7dbbe984843d62089f9b784c484d093a4
Madeleine Yael <myael@student.21-school.ru>
2022-07-29 17:51:36 +0500

    add other example to main

--- ex00/Spring/src/main/java/edu/school21/program/Main.java
+++ ex00/Spring/src/main/java/edu/school21/program/Main.java
@@ -1,6 +1,12 @@
 package edu.school21.program;
 
 import edu.school21.printer.Printer;
+import edu.school21.printer.PrinterWithPrefixImpl;
+import edu.school21.preprocessor.PreProcessor;
+import edu.school21.preprocessor.PreProcessorToUpperImpl;
+import edu.school21.renderer.Renderer;
+import edu.school21.renderer.RendererErrImpl;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,6 +23,12 @@ public class Main {
         Printer printer1 = context.getBean("ppsl", Printer.class);
         printer1.setPrefix("Lets go!");
         printer1.print("NNo");
+
+//        PreProcessor preProcessor = new PreProcessorToUpperImpl();
+//        Renderer renderer = new RendererErrImpl(preProcessor);
+//        PrinterWithPrefixImpl printer3 = new PrinterWithPrefixImpl(renderer);
+//        printer3.setPrefix ("Prefix ");
+//        printer3.print ("Hello!");
     }
 }
 
